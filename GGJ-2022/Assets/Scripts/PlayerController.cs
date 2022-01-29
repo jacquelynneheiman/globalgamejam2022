c68595dc9a27a192377454b5c6b478fd89633b1e
amoore-uat <amoore@uat.edu>
2022-01-29 11:20:40 -0700

    PlayerController Move method implemented

--- GGJ-2022/Assets/Scripts/PlayerController.cs
+++ GGJ-2022/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 1f;
+    CharacterController characterController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move()
     {
-        
+        Vector3 moveVector = transform.forward * moveSpeed;
+        characterController.SimpleMove(moveVector);
     }
 }
