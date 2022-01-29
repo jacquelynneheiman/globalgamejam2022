a61425c6e684845481d9b555b772c9921dfd3b50
amoore-uat <amoore@uat.edu>
2022-01-29 15:15:12 -0700

    PlayerPrefab updated. Mouse Look implemented.

--- GGJ-2022/Assets/Scripts/PlayerController.cs
+++ GGJ-2022/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float lookSpeed = 400f;
+
     CharacterController characterController;
+    Camera playerCamera;
 
     // Start is called before the first frame update
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         characterController = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
+        if (playerCamera == null) { Debug.LogWarning("[PlayerController] couldn't find player camera."); }
     }
 
     private void Update()
     {
+        // Movement
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        Look(mouseX, mouseY);
+
+        // Mouse Look
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Move(horizontalInput, verticalInput);
@@ -23,9 +36,18 @@ public class PlayerController : MonoBehaviour
 
     public void Move(float horizontal, float vertical)
     {
-        float forwardMovement = vertical * moveSpeed;
-        float lateralMovement = horizontal * moveSpeed;
-        Vector3 moveVector = new Vector3(lateralMovement, 0, forwardMovement);
+        Vector3 forwardMovement = vertical * moveSpeed * transform.forward;
+        Vector3 lateralMovement = horizontal * moveSpeed * transform.right;
+        Vector3 moveVector = forwardMovement + lateralMovement;
         characterController.SimpleMove(moveVector);
+    }
+
+    public void Look(float horizontal, float vertical)
+    {
+        Vector3 horizontalRotateVector = Vector3.up * horizontal * lookSpeed * Time.deltaTime;
+        transform.Rotate(horizontalRotateVector, Space.Self);
+
+        Vector3 verticalRotateVector = Vector3.right * vertical * lookSpeed * Time.deltaTime * -1f;
+        playerCamera.transform.Rotate(verticalRotateVector, Space.Self);
     }
 }
