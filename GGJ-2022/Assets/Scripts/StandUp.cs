3eb8348332e94a2018cb26e8ef3e6887e4f743f0
amoore-uat <amoore@uat.edu>
2022-01-30 14:46:33 -0700

    final audio

--- GGJ-2022/Assets/Scripts/StandUp.cs
+++ GGJ-2022/Assets/Scripts/StandUp.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class StandUp : MonoBehaviour
 {
+    public AudioSource deskThrow;
+    public AudioSource wtf;
     public Animator animator;
     public float standDelay;
+    public float soundDelay = 3.5f;
 
     public void Stand()
     {
@@ -15,7 +18,17 @@ public class StandUp : MonoBehaviour
     IEnumerator StandCo()
     {
         yield return new WaitForSeconds(standDelay);
-
+        if (wtf != null)
+        {
+            if (!wtf.isPlaying)
+            {
+                wtf.Play();
+            }
+        }
+        if (deskThrow != null)
+        {
+            deskThrow.PlayDelayed(soundDelay);
+        }
         animator.SetBool("stand", true);
     }
 }
