cc3066baab55311fed431a54ceb73fd28a2715ae
amoore-uat <amoore@uat.edu>
2022-01-29 19:52:45 -0700

    Events class with example created

---
+++ GGJ-2022/Assets/Scripts/Events.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Events
+{
+    // Use the below example to create custom events.
+    //[System.Serializable] public class EventGameStateChanged : UnityEvent<GameManager.GameState, GameManager.GameState> { }
+}
