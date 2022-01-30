3f8149b8428ec0cc1f80e5edb3ed4e7fa8901430
jacquelynneheiman <77410423+jacquelynneheiman@users.noreply.github.com>
2022-01-30 09:07:30 -0800

    Cleaned up the scene and made some trigger scripts

---
+++ GGJ-2022/Assets/Scripts/FlickerLights.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLights : MonoBehaviour
+{
+    public List<Light> lights = new List<Light>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerController>())
+        {
+            foreach(Light light in lights)
+            {
+                light.GetComponent<Animator>().SetTrigger("Flicker");
+            }
+        }
+    }
+}
